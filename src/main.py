3ccdbf5a89f6e8343dfce353d5f330d03ecc19a2
Morgan O'Sullivan <yongliang1000@gmail.com>
2023-11-02 14:08:54 -0700

    Created the queue system? And created the basic Webquery endpoint

--- src/main.py
+++ src/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI, Body
 from fastapi import HTTPException
 import asyncio
 from pydantic import BaseModel
+from queue import Queue
+from uuid import uuid4
+
 
 import queue
 
@@ -41,9 +44,129 @@ class ClearChatHistory(BaseModel):
     minutesID: str
     chatHistoryID: str 
 
+class WebQueryQuestion(BaseModel):
+    question: str
+
+
 
 
 app = FastAPI()
+
+# So mostly done by chatgpt lmao
+# But essentially, we create this function that just handles things in the queue
+
+@app.on_event("startup")
+async def start_worker():
+    asyncio.create_task(worker())
+    print("worker started")
+
+# The queue of task and a dictionary with their id and results
+task_queue = Queue()
+pending_tasks = {}
+
+
+stop_signal = False
+
+# Honestly I have no idea why this is needed but oh well
+@app.on_event("shutdown")
+async def stop_worker():
+    global stop_signal
+    stop_signal = True
+
+
+
+async def worker():
+    while not stop_signal:
+        if not task_queue.empty():
+            task_type, task_data, task_id = task_queue.get()
+            event, _ = pending_tasks[task_id]
+            
+            try:
+                if task_type == 'update_agenda':
+                    mongoDB = MongoDBManager(task_data.minutesID, task_data.chatHistoryID)
+                    result = await mongoDB.update_agenda_meeting(task_data.agenda, True) 
+
+                elif task_type == 'track_minutes':
+                    result = await track_minutes(task_data.minutes, task_data.topicTitle, task_data.topicID, task_data.minutesID, task_data.chatHistoryID, task_data.abbreviation)
+                
+                elif task_type == 'update_meeting':
+                    mongoDB = MongoDBManager(task_data.minutesID, task_data.chatHistoryID)
+                    result = await mongoDB.update_agenda_meeting(task_data.data, False) 
+                
+                elif task_type == 'delete_topic':
+                    mongoDB = MongoDBManager(task_data.minutesID, task_data.chatHistoryID)
+                    result =  await mongoDB.delete_topic(task_data.topicID)
+                
+                elif task_type == 'clear':
+                    mongoDB = MongoDBManager(task_data.minutesID, task_data.chatHistoryID)
+                    result = await mongoDB.clear_chat_history(task_data.type)
+
+                elif task_type == 'webquery':
+                    result = await WebQuery(task_data.question)
+                
+                # Store the result
+                _, _ = pending_tasks[task_id]
+                pending_tasks[task_id] = (event, result)
+            
+            except Exception as e:
+                # You can handle exceptions here, and potentially return a meaningful error to the caller
+                pending_tasks[task_id] = (event, {"error": str(e)})
+            
+            finally:
+                # Signal that the task is done
+                event.set()
+
+        else:
+            await asyncio.sleep(1)
+
+
+# Function used to put things in a queue and wait for response
+
+async def AddTaskWaitResponse(task_type: str, task_data):
+    task_id = uuid4().hex
+    event = asyncio.Event()
+    task = (task_type, task_data, task_id)
+    task_queue.put(task)
+    pending_tasks[task_id] = (event, None)  # The second element is for the result, which is None initially
+    
+    await event.wait()
+    result = pending_tasks.pop(task_id)[1]
+    
+    return result
+
+
+# Update_agenda and track_minutes have been updated, they will now just put things into the queue
+# Wait for the worker to work on them and when there is completion, take the result and return
+# They also clear their result from pending task
+@app.post("/update_agenda")
+async def update_agenda(request_body: AgendaUpdateRequest):
+    return await AddTaskWaitResponse('update_agenda', request_body)
+
+@app.post("/track_minutes")
+async def handle_track_minutes(request_body: TrackMinutesRequest):
+    return await AddTaskWaitResponse('track_minutes', request_body)
+
+@app.post("/update_meeting")
+async def update_meeting(request_body: MeetingUpdateRequest):
+    return await AddTaskWaitResponse('update_meeting', request_body)
+
+@app.post("/delete_topic")
+async def handle_delete_topic(request_body: DeleteTopicRequest):
+    return await AddTaskWaitResponse('delete_meeting', request_body)
+
+@app.post("/clear")
+async def handle_clear_chat(request_body:ClearChatHistory):
+    return await AddTaskWaitResponse('clear', request_body)
+
+@app.post("/webquery")
+async def handleWebQuery(request_body:WebQueryQuestion):
+    return await AddTaskWaitResponse('webquery', request_body)
+
+
+
+
+
+# I have no clue if we should keep these functions as are or just move them into the worker
 
 @app.get("/")
 async def root():
@@ -53,36 +176,6 @@ async def root():
 @app.get("/create")
 async def create_document():
     return initialiseMongoData()
-
-
-@app.post("/update_agenda")
-async def update_agenda(request_body: AgendaUpdateRequest):
-    mongoDB = MongoDBManager(request_body.minutesID, request_body.chatHistoryID)
-    return await mongoDB.update_agenda_meeting(request_body.agenda, True) 
-
-
-@app.post("/update_meeting")
-async def update_meeting(request_body: MeetingUpdateRequest):
-    mongoDB = MongoDBManager(request_body.minutesID, request_body.chatHistoryID)
-    return await mongoDB.update_agenda_meeting(request_body.data, False) 
-
-
-@app.post("/track_minutes")
-async def handle_track_minutes(request_body: TrackMinutesRequest):
-    return await track_minutes(request_body.minutes, request_body.topicTitle, request_body.topicID, request_body.minutesID, request_body.chatHistoryID, request_body.abbreviation)
-
-
-@app.post("/delete_topic")
-async def handle_delete_topic(request_body: DeleteTopicRequest):
-    mongoDB = MongoDBManager(request_body.minutesID, request_body.chatHistoryID)
-    return await mongoDB.delete_topic(request_body.topicID)
-
-
-@app.post("/clear")
-async def handle_clear_chat(request_body:ClearChatHistory):
-        mongoDB = MongoDBManager(request_body.minutesID, request_body.chatHistoryID)
-        return await mongoDB.clear_chat_history(request_body.type)
-
 
 
 ##for our personal use, should never be called by frontend
@@ -98,4 +191,33 @@ async def handle_delete_document(collectionName: str = Body(...), documentID: st
 async def handle_delete_collection(collectionName: str = Body(...), minutesID: str = Body(...), chatHistoryID: str = Body(...)):
     mongoDB = MongoDBManager(minutesID, chatHistoryID)
     return await mongoDB.delete_all_documents(collectionName)
+
+# These functions have been moved into the worker
+
+# @app.post("/track_minutes")
+# async def handle_track_minutes(request_body: TrackMinutesRequest):
+#     return await track_minutes(request_body.minutes, request_body.topicTitle, request_body.topicID, request_body.minutesID, request_body.chatHistoryID, request_body.abbreviation)
+
+# @app.post("/update_agenda")
+# async def update_agenda(request_body: AgendaUpdateRequest):
+#     mongoDB = MongoDBManager(request_body.minutesID, request_body.chatHistoryID)
+#     return await mongoDB.update_agenda_meeting(request_body.agenda, True) 
+
+
+# Untested cause I forgot the input json format lmao
+# @app.post("/update_meeting")
+# async def update_meeting(request_body: MeetingUpdateRequest):
+#     mongoDB = MongoDBManager(request_body.minutesID, request_body.chatHistoryID)
+#     return await mongoDB.update_agenda_meeting(request_body.data, False) 
+
+# @app.post("/delete_topic")
+# async def handle_delete_topic(request_body: DeleteTopicRequest):
+#     mongoDB = MongoDBManager(request_body.minutesID, request_body.chatHistoryID)
+#     return await mongoDB.delete_topic(request_body.topicID)
+
+
+# @app.post("/clear")
+# async def handle_clear_chat(request_body:ClearChatHistory):
+#         mongoDB = MongoDBManager(request_body.minutesID, request_body.chatHistoryID)
+#         return await mongoDB.clear_chat_history(request_body.type)
 
