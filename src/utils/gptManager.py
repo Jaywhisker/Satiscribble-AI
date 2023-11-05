dcfd20eaa3f9130f586e22c14ea4ab14fce9eb57
Jaywhisker <51687528+Jaywhisker@users.noreply.github.com>
2023-11-05 17:10:13 +0800

    functioning streaming for document and web qna

--- src/utils/gptManager.py
+++ src/utils/gptManager.py
@@ -1,21 +1,23 @@
+import time
 from fastapi import HTTPException
 import openai
 import os
-import string
+from starlette.responses import StreamingResponse
+import asyncio
 
 async def queryGPT(query:list, model:str='gpt-3.5-turbo', temperature:float=0.2, request_timeout:int=3, max_retries:int=3):
     """
-    General function to query gpt while maintaining timeout
+        General function to query gpt while maintaining timeout. Only returns full response
 
-    Args:
-        query (list): your query to gpt
-        model (str): name of model you are using, defults gpt-3.5-turbo
-        temperature (int): what temperature are you setting, defaults 0.2
-        request_timeout (int): maximum time (in seconds) before timeout error
-        max_retries (int): maximum number of retries in the case of timeout
+        Args:
+            query (list): your query to gpt
+            model (str): name of model you are using, defults gpt-3.5-turbo
+            temperature (int): what temperature are you setting, defaults 0.2
+            request_timeout (int): maximum time (in seconds) before timeout error
+            max_retries (int): maximum number of retries in the case of timeout
 
-    Returns:
-        gpt response
+        Returns:
+            gpt response
     """
     openai.api_key = os.environ['OPENAI_API_KEY']
     retry_count = 0
@@ -25,11 +27,18 @@ async def queryGPT(query:list, model:str='gpt-3.5-turbo', temperature:float=0.2,
                 model=model,
                 messages=query,
                 temperature=temperature,
-                request_timeout=request_timeout
+                request_timeout=request_timeout,
             )
+
+            # if stream:
+            #     return StreamingResponse(streamGenerator(response),
+            #                             media_type='text/event-stream',
+            #                             headers=header)
+
             return response['choices'][0]['message']['content']
         
         except Exception as e:
+            print(e)
             retry_count += 1
             if retry_count <= max_retries:
                 print(f"Request failed (Retry {retry_count}). Pausing for 1 second before retrying.")
@@ -40,16 +49,88 @@ async def queryGPT(query:list, model:str='gpt-3.5-turbo', temperature:float=0.2,
 
 
 
+def streamGPTQuery(query:list, user_query:str, type:str, model:str='gpt-3.5-turbo', temperature:float=0.2, request_timeout:int=3, max_retries:int=3, header = None, mongoDB = None):
+    """
+        Function to query gpt and return STREAMED response
+
+        Args:
+            query (list): formatted user query with context to be passed to gpt
+            user_query (str): formatted user query alone (no context) to be passed to mongoDB
+            type (str): either 'document' / 'web', determine query type
+            model (str, optional): gpt model to be used. Defaults to 'gpt-3.5-turbo'.
+            temperature (float, optional): temperature of model. Defaults to 0.2.
+            request_timeout (int, optional): time in seconds before a response timeouts. Defaults to 3.
+            max_retries (int, optional): maximum number of retries before throwing an error. Defaults to 3.
+            header (_type_, optional): header to be added to the response. Defaults to None.
+            mongoDB (_type_, optional): mongoDB instance to allow for updating of chatHistory. Defaults to None.
+
+        Returns:
+            streamingResponse for fastAPI
+    """
+    openai.api_key = os.environ['OPENAI_API_KEY']
+    retry_count = 0
+    while retry_count <= max_retries:
+        try:
+            response = openai.ChatCompletion.create(
+                model=model,
+                messages=query,
+                temperature=temperature,
+                request_timeout=request_timeout,
+                stream = True
+            )
+
+            return StreamingResponse(streamGenerator(response, mongoDB, user_query, type),
+                                    media_type='text/event-stream',
+                                    headers=header)
+        
+        except Exception as e:
+            print(e)
+            retry_count += 1
+            if retry_count <= max_retries:
+                print(f"Request failed (Retry {retry_count}). Pausing for 1 second before retrying.")
+                time.sleep(1)
+            else:
+                print("Max retries reached. Returning an error.")
+                raise HTTPException(status_code=500, detail="GPT timeout, please check GPT server.")
+
+
+async def streamGenerator(response, mongoDB, user_query, type):
+    """
+        Function to prepare response for streaming
+        If the response ended (by having chunk['choices'][0]['delta'] == {}), update mongoDB
+    
+        Args:
+            response (streaming_chunk): chunk of streaming response
+            mongoDB: instance of mongoDB for updating chatHistory
+            user_query: original user query
+            type: document / web for mongoDB to udpate
+    """
+    full_response = ''
+    try:
+        for chunk in response:
+            if chunk['choices'][0]['delta'] != {}:
+                yield chunk['choices'][0]['delta']['content']
+                full_response += chunk['choices'][0]['delta']['content']
+                await asyncio.sleep(0.1)
+            else:
+                if mongoDB:
+                    query_resp_pair = {'user': user_query, 'assistant': full_response}
+                    await mongoDB.update_chat_history(query_resp_pair, type)
+    except asyncio.TimeoutError:
+        raise HTTPException(status_code=504, detail="Stream timed out")
+    
+
+
 async def TopicTracker(context: list):
     """
-    Return True or False on whether the current sentence is still cohernt with the rest of the paragraph.
+        Return True or False on whether the current sentence is still cohernt with the rest of the paragraph.
 
-    Args:
-        context: the "min_context" number of sentences to be used as context for chatgpt
+        Args:
+            context: the "min_context" number of sentences to be used as context for chatgpt
 
-    Return:
-        True or False 
-        if Chatgpt returns something other than T/F, then we will print a statement and take as False
+        Return:
+            True or False 
+            if Chatgpt returns something other than T/F, then we will print a statement and take as False
     """
     if len(context) <= 1:
         return True
@@ -77,14 +158,14 @@ async def TopicTracker(context: list):
 
 async def AgendaTracker(context: list, agenda: list):
     """
-    Return True or False on whether the current sentence is still cohernt with the agenda of the meeting.
+        Return True or False on whether the current sentence is still cohernt with the agenda of the meeting.
 
-    Args:
-        context: the "min_context" number of sentences to be used as context for chatgpt
-        agenda: the list of agenda items
-    Return:
-        True or False 
-        if Chatgpt returns something other than T/F, then we will print a statement and take as False
+        Args:
+            context: the "min_context" number of sentences to be used as context for chatgpt
+            agenda: the list of agenda items
+        Return:
+            True or False 
+            if Chatgpt returns something other than T/F, then we will print a statement and take as False
     """
     if len(context) <= 1:
         return True
@@ -97,8 +178,6 @@ async def AgendaTracker(context: list, agenda: list):
     user_input = {"role": "user", "content": "AgendaItems:" + str(agenda) + ", Sentences:" + sentences}
     query_message.append(user_input)
     response = await queryGPT(query_message, request_timeout=5)
-    # await asyncio.sleep(5)
-    # print("TopicTrackerResponse")
     ###
     if response == "True":
         return True
@@ -112,13 +191,14 @@ async def AgendaTracker(context: list, agenda: list):
 
 async def GlossaryDetector(context: list, abbreviation:str):
     """
-    Return what the abbreviation stands for in the context of the sentences provided
+        Return what the abbreviation stands for in the context of the sentences provided
 
-    Args:
-        context: the "min_context" number of sentences to be used as context for chatgpt
-        abbreviation: the abbreviation to be defined
-    Return:
-        number of words equivilant to the number of letters in the Abbreviation provided    """
+        Args:
+            context: the "min_context" number of sentences to be used as context for chatgpt
+            abbreviation: the abbreviation to be defined
+        Return:
+            number of words equivilant to the number of letters in the Abbreviation provided    ]
+    """
     if abbreviation == None:
         return None
     else:
@@ -130,13 +210,7 @@ async def GlossaryDetector(context: list, abbreviation:str):
         ]
         user_input = {"role": "user", "content": "Abbreviation: " + abbreviation + ", Context:" + sentences}
         query_message.append(user_input)
-        # response = openai.ChatCompletion.create(
-        #     model="gpt-3.5-turbo",
-        #     messages= query_message,
-        #     temperature=0.2
-        #     )
-        # await asyncio.sleep(5)
-        # print("TopicTrackerResponse")
+
         response = await queryGPT(query_message, request_timeout=5)
 
         return response.strip().rstrip('.')
@@ -145,15 +219,15 @@ async def GlossaryDetector(context: list, abbreviation:str):
 
 async def createStandAloneQuery(formatted_chat_history:list, query:str):
     """
-    Function to convert the latest user query to a standalone question such that it can be used to query to the chromaDB
-    Will only take the last 3 user input and user results
+        Function to convert the latest user query to a standalone question such that it can be used to query to the chromaDB
+        Will only take the last 3 user input and user results
 
-    Args:
-        formatted_chat_history (list): chat history that will be used as context 
-        query (string): user query that will be reformatted
-    
-    Returns:
-        formatted query to become a standalone question
+        Args:
+            formatted_chat_history (list): chat history that will be used as context 
+            query (string): user query that will be reformatted
+        
+        Returns:
+            formatted query to become a standalone question
     """
     #only taking latest 3 user query
     if len(formatted_chat_history) > 6:
@@ -182,16 +256,17 @@ async def createStandAloneQuery(formatted_chat_history:list, query:str):
 
 
 
-async def documentQuery(query:str, context_dict:dict):
+async def documentQuery(query:str, context_dict:dict, header:dict):
     """
-    Function to query GPT based on the minutes
+        Function to query GPT based on the minutes
 
-    Args:
-        query (string): reformatted user query
-        context_dict (list): dictionary containing the context for gpt. In the format of {topic_title: topic_details}
+        Args:
+            query (string): reformatted user query
+            context_dict (list): dictionary containing the context for gpt. In the format of {topic_title: topic_details}
+            header (dict): headers that contain the source_id to be sent back
 
-    Returns:
-        answer to the question
+        Returns:
+            answer to the question
     """
     format_context = ''
     for topic_title, topic_text in context_dict.items():
@@ -206,28 +281,27 @@ async def documentQuery(query:str, context_dict:dict):
                         {query}
                     """
     query_message = [{"role": "system", "content": system_prompt}]
-    response =  await queryGPT(query_message, request_timeout=5)
-    return response.strip()
+    return query_message
+
 
 
 async def webQuery(query:str, context:list):
     """
-    Function to query GPT based on the conversation you had with it
+        Function to query GPT based on the conversation you had with it
 
-    Args:
-        query (string): reformatted user query
-        context_dict (list): dictionary containing the context for gpt. In the format of {topic_title: topic_details}
+        Args:
+            query (string): reformatted user query
+            context_dict (list): dictionary containing the context for gpt. In the format of {topic_title: topic_details}
 
-    Returns:
-        answer to the question
+        Returns:
+            formatted query message
     """
     query_message = [
-    {"role": "system", "content": "You are an Simple question and answer Model. You do not have individuality, opinion or a personality. You will receive a question. Answer the question in the most straight forwawrd way possible. Minimising words where possible. Try and keep responses below 50 words."},
-    ]
+            {"role": "system", "content": "You are an Simple question and answer Model. You do not have individuality, opinion or a personality. You will receive a question. Answer the question in the most straight forwawrd way possible. Minimising words where possible. Try and keep responses below 50 words."},
+        ]
     query_message = query_message + context
     query_message.append({"role": "user", "content": query})
-    response = await queryGPT(query_message, request_timeout=5)
-    return response.strip()
+    return query_message
 
 
 
