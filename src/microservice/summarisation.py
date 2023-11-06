6e6d86bb4cbd87bcb8e4ee879aa1b579edab4993
bcjunxiang <totallybryancheong@gmail.com>
2023-11-06 20:55:06 +0800

    Update summarisation.py

--- src/microservice/summarisation.py
+++ src/microservice/summarisation.py
@@ -14,11 +14,17 @@ async def summariseText(topic_title: str, topic_id: str, minutes_id: str,chat_hi
     
     content = mongoDB.read_MongoDB('minutes', False, topic_title, None)
 
+    #format minutes into a single string
     formatted_minutes=formatPreSummaryMinutes(content, topic_title)
+
+    #check if string is empty
+    if not formatted_minutes.strip():
+        return {"summary": "Nothing to summarise."}
+
     #prepare prompt for OpenAI
     query_message=[{"role": "system", "content": "You are a helpful summarizing model called CuriousCat. You do not have individuality, opinion or a personality. Please summarize the following paragraph in a maximum of 2 sentences or less. Make it concise, with all important details retained. The summary must have a MAX WORD LIMIT OF 80 WORDS, focusing on readability."},]
     query_message.append({"role": "user", "content": formatted_minutes})
     
-
+    #query for response and return json format
     response = await queryGPT(query_message)
     return {"summary": response}
