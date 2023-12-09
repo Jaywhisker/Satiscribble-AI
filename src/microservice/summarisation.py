bdb5529994d8696b6fb4d7f23c2ff9e39bd6c484
bcjunxiang <totallybryancheong@gmail.com>
2023-12-10 01:00:45 +0800

    Update summarisation.py

--- src/microservice/summarisation.py
+++ src/microservice/summarisation.py
@@ -22,9 +22,9 @@ async def summariseText(minutes_id: str, chat_history_id: str, topic_id: str):
         return {"summary": "Nothing to summarise."}
 
     #prepare prompt for OpenAI
-    query_message=[{"role": "system", "content": "You are a helpful summarizing model called CuriousCat. You do not have individuality, opinion or a personality. Please summarize the following paragraph in a maximum of 2 sentences or less. Make it concise, with all important details retained. The summary must have a MAX WORD LIMIT OF 80 WORDS, focusing on readability."},]
+    query_message=[{"role": "system", "content": "Please read through the following paragraph. Please SUMMARISE the content of THE MOST IMPORTANT sentences into ONE single COHESIVE and EXTREMELY SHORT sentence topic description. The RESPONSE should be as CONCISE and COMPREHENSIVE as possible so as to cover as much content in AS FEW WORDS as possible. Your generated response must NEVER be LONGER than the given paragraph, and it must NEVER EXCEED 60 words.It must only be ONE sentence please. Please take A THIRD PERSON POINT OF VIEW. ALWAYS return only the one sentence description. NEVER preface the RESPONSE and just PURELY GIVE me the response directly. Please give your response from a THIRD PERSON PERSPECTIVE."},]
     query_message.append({"role": "user", "content": formatted_minutes})
-    
+
     #query for response and return json format
     response = await queryGPT(query=query_message, request_timeout=5)
     return {"summary": response}
