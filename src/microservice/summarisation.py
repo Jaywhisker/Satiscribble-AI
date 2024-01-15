3c08ac616bdfa9e12b8998552c5c5e45534951fe
bcjunxiang <totallybryancheong@gmail.com>
2024-01-15 12:29:02 +0800

    Update summarisation.py

--- src/microservice/summarisation.py
+++ src/microservice/summarisation.py
@@ -22,7 +22,16 @@ async def summariseText(minutes_id: str, chat_history_id: str, topic_id: str):
         return {"summary": "Nothing to summarise."}
 
     #prepare prompt for OpenAI
-    query_message=[{"role": "system", "content": "Please read through the following paragraph. Please SUMMARISE the content of THE MOST IMPORTANT sentences into ONE single COHESIVE and EXTREMELY SHORT sentence topic description. The RESPONSE should be as CONCISE and COMPREHENSIVE as possible so as to cover as much content in AS FEW WORDS as possible. Your generated response must NEVER be LONGER than the given paragraph, and it must NEVER EXCEED 60 words.It must only be ONE sentence please. Please take A THIRD PERSON POINT OF VIEW. ALWAYS return only the one sentence description. NEVER preface the RESPONSE and just PURELY GIVE me the response directly. Please give your response from a THIRD PERSON PERSPECTIVE."},]
+    query_message=[{"role": "system", "content": """
+                    Please read through the following paragraph.
+                    Please SUMMARISE the content of THE MOST IMPORTANT sentences into ONE single COHESIVE and EXTREMELY SHORT sentence topic description.
+                    The RESPONSE should be as CONCISE and COMPREHENSIVE as possible so as to cover as much content in AS FEW WORDS as possible. 
+                    Your generated response must NEVER be LONGER than the given paragraph, and in at most 60 words. 
+                    It must only be ONE sentence please. Please take A THIRD PERSON POINT OF VIEW. 
+                    ALWAYS return only the one sentence description. 
+                    NEVER preface the RESPONSE and just PURELY GIVE me the response directly. 
+                    Please give your response from a THIRD PERSON PERSPECTIVE.
+                    """},]
     query_message.append({"role": "user", "content": formatted_minutes})
 
     #query for response and return json format
